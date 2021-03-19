cab027115978e2c1966169c2d000cb32111f7354
AlyAsuncion <79674793+AlyAsuncion@users.noreply.github.com>
2021-03-20 07:19:06 +0800

    Add files via upload

---
+++ Asuncion.java
@@ -0,0 +1,24 @@
+package studentinfo;
+
+public class function {
+
+	private String name2, address2, course2;
+	private int age2;
+
+public  String nameAsuncion() {
+   return name2 ="Allyssa Asuncion";    
+}
+
+public String addresssAsuncion(){
+return address2 "Camiling Tarlac";
+}
+
+public String courseAsuncion(){
+return course2= "BS Info Tech";
+}
+
+public int ageAsuncion(){ 
+return age2= 21;
+}
+
+}
