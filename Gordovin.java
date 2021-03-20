dd883f70adf1f7da311152423055df1887f05892
Harley0321 <79955098+Harley0321@users.noreply.github.com>
2021-03-20 15:43:40 +0800

    Adding four methods

---
+++ Gordovin.java
@@ -0,0 +1,23 @@
+package StudentInfo;
+
+public class function {
+
+Private string name7,addres7;course7;
+Private int age7;
+
+public  String NameHarley() {
+   return name7 ="Harley Gordovin Jr.";    
+}
+
+public String addressHarley(){
+return address7= "Mangatarem,Pangasinan";
+}
+
+public String courseHarley(){
+return course4= "BSIT-NA";
+}
+
+public int ageHarley(){ 
+return age7= 23;
+}
+} 
